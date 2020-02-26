e288d7c59baa0d54b0aa97d8638f953b61f7230f
Tan Vu <vtan502262@gmail.com>
2020-02-26 09:22:24 -0600

    Add some function to controllers, made ticket model

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,1 +1,14 @@
-let Flight = require('../models/flight');
+const Flight = require('../models/flight');
+
+module.exports = {
+    new: newFlight,
+    index
+};
+
+function index(req, res){
+   res.render('flights/index'); 
+}
+function newFlight(req, res) {
+    res.render('flights/new');
+};
+
